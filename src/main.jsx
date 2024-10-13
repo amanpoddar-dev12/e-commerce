a7075dce12afa05e9360684c1aa209da85525cba
amanpoddar-dev12 <amanpoddar775@gmail.com>
2024-10-14 03:07:56 +0530

    add

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import ThemeProvider from "./themeContext/Theme.jsx";
 import { Provider } from "react-redux";
-import store from "./store/store.js";
+import store from "./redux/store.js";
 
 import {
   // BrowserRouter,
