fb0ca3485940ad8db01b2c1e6a1cc5666a07e488
amanpoddar-dev12 <amanpoddar775@gmail.com>
2024-10-14 02:36:22 +0530

    add store

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import ThemeProvider from "./themeContext/Theme.jsx";
 import { Provider } from "react-redux";
-import store from "./store/store.js"; // Ensure this path matches exactly
+import store from "./store/store.js";
 
 import {
   // BrowserRouter,
