eef92d79abf06bb113f334c2532c6f6433030ae1
amanpoddar-dev12 <amanpoddar775@gmail.com>
2024-10-24 08:42:20 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect } from "react";
 import ProductItem from "../components/ProductItem";
 import { useDispatch, useSelector } from "react-redux";
 import { getProductsData, useGetProductsQuery } from "../Features/productSlice";
