268164d61b263189e797cf7303382eb7f66e899b
amanpoddar-dev12 <amanpoddar775@gmail.com>
2025-01-08 00:17:02 +0530

    commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ function Home() {
   return (
     <div className="h-[700px]">
       <div className=" dark:text-white  md:text-5xl  text-3xl text-center flex flex-col justify-center md:mt-14 lg:mt-26 mt-20 gap-10 font-bold">
-        <p className="md:text-9xl text-7xl mr-0 ">Jestify</p>
+        <p className="md:text-9xl text-7xl mr-0 ">Zestify</p>
         <p className="">"Where Every Click Adds Spark to Your Style!"</p>
       </div>
       <div className="flex flex-wrap gap-3 mt-16 items-center justify-center text-3xl md:text-5xl">
